ad328d15ad4dedd4fbb768e193e9be62399b2786
Letícia Calixto <leticia.calixto@gec.inatel.br>
2022-11-09 11:39:01 -0300

    seminario de teste unitario

---
+++ Seminario - Teste Unitario/__tests__/calculator.teste.js
@@ -0,0 +1,28 @@
+const calculator = require('../calculator')
+
+describe('Calculator tests - Happy way', () => {
+    test('sum 1 + 2', () => {
+        expect(calculator.sum(1,2)).toBe(3);
+    })
+
+    test('diff 3 - 1', () => {
+        expect(calculator.diff(3,1)).toBe(2);
+    })
+
+    test('mult 4 * 5', () => {
+        expect(calculator.mult(4,5)).toBe(20);
+    })
+
+    test('div 4 * 5', () => {
+        expect(calculator.div(12,4)).toBe(3);
+    })
+
+})
+
+describe('Calculator tests - Sadness way', () => {
+   
+    test('sum 1 + 2 not 1', () => {
+        expect(calculator.sum(1,2)).not.toBe(1);
+    })
+
+})
